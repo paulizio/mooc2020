2dbaebfdcf551ba7530a2a57061082e20ffeab96
paulizio <pauli.nordman@hotmail.com>
2020-05-17 21:54:22 +0300

    html taulukko kesken

--- unicafe/src/index.js
+++ unicafe/src/index.js
@@ -7,15 +7,32 @@ const Header=()=>(
 const Button=({onClick,text})=>(
   <button onClick={onClick}>{text}</button>
 )
-const Statistics=({good,neutral,bad})=>(
-  <div>
+const Statistics=({good,neutral,bad,all,points})=>{
+  const average=function(a,b){
+    return (a/b)
+  }
+  const positive=function(a,b){
+    return (a/b)*100+'%'
+  }
+  return(
+
+  <table>
+  <tbody>
   <h2>Statistics</h2>
-  
-<li>Good: {good}</li>
-<li>Neutral: {neutral}</li>
-<li>Bad: {bad}</li>
-  </div>
-)
+  <tr>
+<td>Good: {good}</td>
+<td>Neutral: {neutral}</td>
+<td>Bad: {bad}</td>
+<td>All: {all}</td>
+<td>Average: {average(points,all)}</td>
+<td>Positiivisia: {positive(good,all)}</td>
+  </tr>
+  </tbody>
+  </table>
+ 
+  )
+}
+
 
 
 
@@ -24,15 +41,24 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all,setAll]=useState(0)
+  const[points,setPoints]=useState(0)
 
   const clickGood=()=>{
     setGood(good+1)
+    setAll(all+1)
+    console.log(points)
+    setPoints(points+1)
   }
   const clickBad=()=>{
     setBad(bad+1)
+    setAll(all+1)
+    console.log(points)
+    setPoints(points-1)
   }
   const clickNeutral=()=>{
     setNeutral(neutral+1)
+    setAll(all+1)
   }
   return (
     <div>
@@ -40,7 +66,7 @@ const App = () => {
       <Button onClick={clickGood} text='good' />
       <Button onClick={clickNeutral} text='neutral' />
       <Button onClick={clickBad} text='bad' />
-      <Statistics good={good} bad={bad} neutral={neutral}/>
+      <Statistics good={good} bad={bad} neutral={neutral} all={all} points={points}/>
     </div>
   )
 }
