1e1f74e9ba8967a53d4cf65f3b7ca2d254d28689
paulizio <pauli.nordman@hotmail.com>
2020-05-19 21:13:07 +0300

    save

--- puhelinluettelo/src/App.js
+++ puhelinluettelo/src/App.js
@@ -5,20 +5,33 @@ const App = () => {
     { name: 'Arto Hellas' }
   ]) 
   const [ newName, setNewName ] = useState('')
+const addNote=(event)=>{
+  event.preventDefault()
+  const personObject={
+    name:newName
+    
+  }
+  newName.includes(persons.name)?alert('asd'):setPersons(persons.concat(personObject))
+}
+const handleNewNote=(event)=>{
+  setNewName(event.target.value)
+  console.log(event.target.value)
+  
 
+}
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addNote}>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handleNewNote}/>
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      {persons.map(p=><li>{p.name}</li>)}
     </div>
   )
 
