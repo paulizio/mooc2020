03335a5b0d3991a3d47eb96f38dd1af7a5a89692
paulizio <pauli.nordman@hotmail.com>
2020-05-28 21:31:11 +0300

    5.5 wip

---
+++ osa5/src/components/BlogForm.js
@@ -0,0 +1,61 @@
+import React,{useState} from 'react'
+import Blog from './Blog'
+const BlogForm=({createBlog})=>{
+    const [title,setNewTitle]=useState('')
+    const [author,setNewAuthor]=useState('')
+    const [url,setNewUrl]=useState('')
+    const [blogs,SetBlogs]=useState([])
+
+    const handleTitleChange=(event)=>{
+    setNewTitle(event.target.value)
+    }
+    const handleAuthorChange=(event)=>{
+        setNewAuthor(event.target.value)
+        }
+        const handleUrlChange=(event)=>{
+            setNewUrl(event.target.value)
+            }
+    const addBlog=(event)=>{
+        event.preventDefault()
+        createBlog({
+            title:title,
+            author:author,
+            url:url
+        })
+        setNewAuthor('')
+        setNewTitle('')
+        setNewUrl('')
+       
+    }
+    return(
+        <div>
+            <h2>Add new blog</h2>
+
+            <form
+            onSubmit={addBlog}>
+                title
+                <input
+                value={title}
+                onChange={handleTitleChange}
+                />
+                <br/>
+                author
+                <input
+                value={author}
+                onChange={handleAuthorChange}
+                />
+                <br/>
+                url
+                <input
+                value={url}
+                onChange={handleUrlChange}
+                />
+                <br/>
+                <button type="submit">save</button>
+            </form>
+         
+        </div>
+    )
+}
+
+export default BlogForm
