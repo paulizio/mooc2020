3bf071ba2ce642c941ed445d156526b2f9352038
paulizio <pauli.nordman@hotmail.com>
2020-05-28 21:30:28 +0300

    5.5 wip

--- osa5/src/App.js
+++ osa5/src/App.js
@@ -4,6 +4,8 @@ import blogService from './services/blogs'
 import loginService from './services/login'
 import './App.css'
 import Notification from './components/Notification'
+import Togglable from './components/Togglable'
+import BlogForm from './components/BlogForm'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
@@ -15,6 +17,9 @@ const App = () => {
   const [url,setUrl]=useState('')
   const [errorMessage,setErrorMessage]=useState(null)
   const [errorName,setErrorName]=useState(null)
+
+  const blogFormRef=React.createRef()
+
   useEffect(() => {
     blogService.getAll().then(blogs =>
       setBlogs( blogs )
@@ -80,73 +85,48 @@ const loginForm=()=>(
 )
 
 const blogForm=()=>(
-  <div>
-    <form>
-      <div>
-        title:
-      <input
-       type="text"
-       value={title}
-       name="title:"
-       onChange={({target})=>setTitle(target.value)}
-      />
-      </div>
-      <div>
-        author:
-      <input
-       type="text"
-       value={author}
-       name="author:"
-       onChange={({target})=>setAuthor(target.value)}
-       />
-       </div>
-   
-       <div>
-         url:
-      <input
-        type="text"
-       value={url}
-       name="url:"
-       onChange={({target})=>setUrl(target.value)}
-       />
-       </div>
-       <button type="submit" onClick={newBlog}>create</button>
-    </form>
-  {blogs.map(blog =>
-    <Blog key={blog.id} blog={blog} />
-  )}
-  </div>
+  <Togglable buttonLabel='new blog'>
+    <BlogForm createBlog={addBlog}/>
+  </Togglable>
 )
 const handleLogOut=(event)=>{
   event.preventDefault()
   window.localStorage.removeItem('loggedUser')
   setUser(null)
 }
-const newBlog=(event)=>{
-  event.preventDefault()
-  const blogObject={
-    title:title,
-    author:author,
-    url:url,
-  }
-  try{
-  blogService.create(blogObject)
+const addBlog=(blogObject)=>{
+  blogFormRef.current.toggleVisibility()
+  blogService
+  .create(blogObject)
   .then(returnedBlog=>{
     setBlogs(blogs.concat(returnedBlog))
-    setTitle('')
-    setAuthor('')
-    setUrl('')
-    setErrorMessage(`a new blog ${returnedBlog.title} by ${returnedBlog.author} added`)
-    setErrorName('success')
-    setTimeout(()=>{
-      setErrorMessage(null)
-      setErrorName(null)
-    },5000)
-  })}
-catch(exception){
-  setErrorMessage('Adding new blog failed')
+  })
 }
-}
+// const newBlog=(event)=>{
+//   event.preventDefault()
+//   const blogObject={
+//     title:title,
+//     author:author,
+//     url:url,
+//   }
+//   try{
+//   blogService.create(blogObject)
+//   .then(returnedBlog=>{
+//     setBlogs(blogs.concat(returnedBlog))
+//     setTitle('')
+//     setAuthor('')
+//     setUrl('')
+//     setErrorMessage(`a new blog ${returnedBlog.title} by ${returnedBlog.author} added`)
+//     setErrorName('success')
+//     setTimeout(()=>{
+//       setErrorMessage(null)
+//       setErrorName(null)
+//     },5000)
+//   })}
+// catch(exception){
+//   setErrorMessage('Adding new blog failed')
+// }
+// }
 
 
   return (
@@ -157,9 +137,13 @@ catch(exception){
      <h2>blogs</h2>
   <p>{user.name} logged in <button onClick={handleLogOut}>logout</button></p>
   {blogForm()}
+  {blogs.map(blog =>
+    <Blog key={blog.id} blog={blog} />
+  )}
   </div>:
      loginForm()   
     }
+     
     </div>
     
      
