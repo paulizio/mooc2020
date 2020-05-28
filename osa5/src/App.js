0adbf323a0f77e14b325465037e21ccea43e60f7
paulizio <pauli.nordman@hotmail.com>
2020-05-28 12:09:12 +0300

    5.1 done

--- osa5/src/App.js
+++ osa5/src/App.js
@@ -1,19 +1,33 @@
 import React, { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
-
+import loginService from './services/login'
 const App = () => {
   const [blogs, setBlogs] = useState([])
   const [username,setUsername]=useState('')
   const [password,setPassword]=useState('')
+  const [user,setUser]=useState(null)
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
       setBlogs( blogs )
     )  
   }, [])
-  const handleLogin=()=>{
+  const handleLogin=async(event)=>{
+    event.preventDefault()
 
+    try{
+      const user=await loginService.login({
+        username,password,
+      })
+      // blogService.setToken(user.token)
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    }catch (exception){
+      console.log('error')
+    }
+    
   }
 const loginForm=()=>(
   <form onSubmit={handleLogin}>
@@ -39,14 +53,28 @@ const loginForm=()=>(
 
   </form>
 )
+
+const blogForm=()=>(
+  <div>
+  {blogs.map(blog =>
+    <Blog key={blog.id} blog={blog} />
+  )}
+  </div>
+)
+
   return (
     <div>
-      {loginForm()}
-      <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
+     {user===null?
+     loginForm():
+     <div>
+        <h2>blogs</h2>
+     <p>{user.name} logged in</p>
+     {blogForm()}
+     </div>
+    }
     </div>
+    
+     
   )
 }
 
