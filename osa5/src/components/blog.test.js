4354c97a276a8ff1d6df171ad0247f8c3126fe3d
paulizio <pauli.nordman@hotmail.com>
2020-05-30 09:01:55 +0300

    5.13 tehty

---
+++ osa5/src/components/blog.test.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import '@testing-library/jest-dom/extend-expect'
+import {render} from '@testing-library/react'
+import Blog from './Blog'
+
+test('renders title and author but not url or likes',()=>{
+  const blog={
+    title:'Testing some components',
+    author:'Testmaster',
+    url:'testtestest.fi',
+    likes:'20'
+  }
+  const component=render(
+    <Blog blog={blog}/>
+  )
+  expect(component.container).toHaveTextContent(
+    'Testing some components'
+  )
+})
