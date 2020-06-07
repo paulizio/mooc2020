969e4335dbc680e01434d4c2a2efa7d3f46d5e06
paulizio <pauli.nordman@hotmail.com>
2020-06-07 21:15:33 +0300

    7.2 tehty

--- osa7/src/App.js
+++ osa7/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 
 import {
-Switch,Route,Link,
+Switch,Route,Link,useParams
 } from 'react-router-dom'
 
 const Menu = () => {
@@ -16,12 +16,25 @@ const Menu = () => {
     </div>
   )
 }
+const Anecdote=({anecdotes})=>{
+  const id=useParams().id
+  const anecdote=anecdotes.find(a=>a.id===id)
+  return(
+    <div>
+      <h2>{anecdote.content}</h2>
+      <p>has {anecdote.votes} votes</p>
+      <p>for more info see <a href={anecdote.info}>{anecdote.info} </a></p>
+    </div>
+  )
 
-const AnecdoteList = ({ anecdotes }) => (
+}
+
+const AnecdoteList = ({ anecdotes }) =>(
   <div>
     <h2>Anecdotes</h2>
     <ul>
-      {anecdotes.map(anecdote => <li key={anecdote.id} >{anecdote.content}</li>)}
+      {anecdotes.map(anecdote => <li key={anecdote.id} >
+        <Link to={`/anecdotes/${anecdote.id}`}>{anecdote.content}</Link></li>)}
     </ul>
   </div>
 )
@@ -137,6 +150,9 @@ const App = () => {
           <Route path="/about">
           <About/>
           </Route> 
+          <Route path="/anecdotes/:id">
+            <Anecdote anecdotes={anecdotes}/>
+          </Route>
         <Route path="/">
           <AnecdoteList anecdotes={anecdotes}/>
           </Route> 
