699b635cd42214d18e0ffd1c5d466a7e418122c3
paulizio <pauli.nordman@hotmail.com>
2020-06-07 20:59:41 +0300

    7.1 tehty

--- osa7/src/App.js
+++ osa7/src/App.js
@@ -1,14 +1,18 @@
 import React, { useState } from 'react'
 
+import {
+Switch,Route,Link,
+} from 'react-router-dom'
+
 const Menu = () => {
   const padding = {
     paddingRight: 5
   }
   return (
     <div>
-      <a href='#' style={padding}>anecdotes</a>
-      <a href='#' style={padding}>create new</a>
-      <a href='#' style={padding}>about</a>
+      <Link style={padding} to="/">home</Link>
+      <Link style={padding} to="/create">create new</Link>
+      <Link style={padding} to="/about">about</Link>
     </div>
   )
 }
@@ -126,12 +130,20 @@ const App = () => {
     <div>
       <h1>Software anecdotes</h1>
       <Menu />
-      <AnecdoteList anecdotes={anecdotes} />
-      <About />
-      <CreateNew addNew={addNew} />
+      <Switch>
+          <Route path="/create">
+          <CreateNew addNew={addNew}/>
+          </Route> 
+          <Route path="/about">
+          <About/>
+          </Route> 
+        <Route path="/">
+          <AnecdoteList anecdotes={anecdotes}/>
+          </Route> 
+      </Switch>
       <Footer />
     </div>
   )
 }
 
-export default App;
+export default App
