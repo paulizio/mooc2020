735fada5891bd9f358c11873a2de9ac8f2a5384f
paulizio <pauli.nordman@hotmail.com>
2020-06-08 15:34:06 +0300

    fixed 7.2

--- osa7/src/App.js
+++ osa7/src/App.js
@@ -21,7 +21,7 @@ const Anecdote=({anecdotes})=>{
   const anecdote=anecdotes.find(a=>a.id===id)
   return(
     <div>
-      <h2>{anecdote.content}</h2>
+      <h2>{anecdote.content} by {anecdote.author}</h2>
       <p>has {anecdote.votes} votes</p>
       <p>for more info see <a href={anecdote.info}>{anecdote.info} </a></p>
     </div>
