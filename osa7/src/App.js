bf1bcb187406f4e33bce9bef0fad3b6187e6a3e5
paulizio <pauli.nordman@hotmail.com>
2020-06-09 14:55:13 +0300

    7.4 done

--- osa7/src/App.js
+++ osa7/src/App.js
@@ -3,7 +3,7 @@ import React, { useState } from 'react'
 import {
 Switch,Route,Link,useParams, Redirect
 } from 'react-router-dom'
-
+import {useField} from './hooks'
 const Menu = () => {
   const padding = {
     paddingRight: 5
@@ -68,17 +68,20 @@ const Footer = () => (
 )
 
 const CreateNew = (props) => {
-  const [content, setContent] = useState('')
-  const [author, setAuthor] = useState('')
-  const [info, setInfo] = useState('')
+  // const [content, setContent] = useState('')
+  // const [author, setAuthor] = useState('')
+  // const [info, setInfo] = useState('')
+  const content=useField('content')
+  const author=useField('author')
+  const info=useField('info')
 
 
   const handleSubmit = (e) => {
     e.preventDefault()
     props.addNew({
-      content,
-      author,
-      info,
+      content:content.value,
+      author:author.value,
+      info:info.value,
       votes: 0
     })
 
@@ -90,15 +93,15 @@ const CreateNew = (props) => {
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input name='content' value={content} onChange={(e) => setContent(e.target.value)} />
+          <input name='content' value={content.value} onChange={content.onChange} />
         </div>
         <div>
           author
-          <input name='author' value={author} onChange={(e) => setAuthor(e.target.value)} />
+          <input name='author' value={author.value} onChange={author.onChange} />
         </div>
         <div>
           url for more info
-          <input name='info' value={info} onChange={(e)=> setInfo(e.target.value)} />
+          <input name='info' value={info.value} onChange={info.onChange} />
         </div>
         <button>create</button>
       </form>
@@ -157,7 +160,6 @@ const App = () => {
       <Notification notification={notification}/>
       <Switch>
           <Route path="/create">
-          {/* <CreateNew addNew={addNew}/> */}
           {notification? <Redirect to="/"/>:<CreateNew addNew={addNew}/>}
           </Route> 
           <Route path="/about">
