02c0aac2bc5640f178cc97b9e2ae3665c43972ad
paulizio <pauli.nordman@hotmail.com>
2020-07-04 19:32:09 +0300

    9.3 done

--- osa9/bmiCalculator.ts
+++ osa9/bmiCalculator.ts
@@ -1,4 +1,20 @@
+interface BmiValues{
+    weight:number;
+    height:number;
+}
+const parseArguments=(args:Array<string>):BmiValues=>{
+    if(args.length<4) throw new Error('Not enough arguments');
+    if (args.length>4)throw new Error('Too many arguments');
 
+    if(!isNaN(Number(args[2]))&&!isNaN(Number(args[3]))){
+        return{
+            weight:Number(args[2]),
+            height:Number(args[3])
+        }
+    }else{
+        throw new Error('Provided values are not numbers')
+    }
+}
 const calculateBmi=(weight:number,height:number)=>{
     if(weight/Math.pow(height*0.01,2)>25){
        return 'Overweight (bad weight)'
@@ -9,7 +25,8 @@ const calculateBmi=(weight:number,height:number)=>{
         }
 }
 try{
-    console.log(calculateBmi(74,180));
+    const {weight,height}=parseArguments(process.argv);
+    console.log(calculateBmi(weight,height))
 
 }catch(e){
     console.log('Error, something went wrong: ',e.message)
