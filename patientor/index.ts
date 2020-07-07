d0e3c1e7f094393254734c7d7b1f4209d9a0be06
paulizio <pauli.nordman@hotmail.com>
2020-07-07 14:12:36 +0300

    9.9 done

--- patientor/index.ts
+++ patientor/index.ts
@@ -1,14 +1,18 @@
-import express from 'express'
-const app=express();
-app.use(express.json())
+import express from 'express';
 
-const PORT=3000
+import cors from 'cors';
+const app=express();
+app.use(express.json());
+app.use(cors());
+
+
 
 app.get('/ping',(_req,res)=>{
     console.log('someone pinged here');
     res.send('pong');
-})
+});
 
+const PORT=3001;
 app.listen(PORT,()=>{
-    console.log(`Server running on port ${PORT}`)
-})
+    console.log(`Server running on port ${PORT}`);
+});
