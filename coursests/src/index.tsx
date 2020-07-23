11af7570df28d9e8e5349e7e198c88f3cd7c2b16
paulizio <pauli.nordman@hotmail.com>
2020-07-23 16:50:33 +0300

    9.15

--- coursests/src/index.tsx
+++ coursests/src/index.tsx
@@ -1,29 +1,43 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import {CoursePartsProps} from './types'
+import {CoursePartsProps,CoursePart} from './types'
 const App: React.FC = () => {
+
   const courseName = "Half Stack application development";
-  const courseParts = [
+  const courseParts: CoursePart[] = [
     {
       name: "Fundamentals",
-      exerciseCount: 10
+      exerciseCount: 10,
+      description: "This is an awesome course part"
     },
     {
       name: "Using props to pass data",
-      exerciseCount: 7
+      exerciseCount: 7,
+      groupProjectCount: 3
     },
     {
       name: "Deeper type usage",
-      exerciseCount: 14
+      exerciseCount: 14,
+      description: "Confusing description",
+      exerciseSubmissionLink: "https://fake-exercise-submit.made-up-url.dev"
+    },
+    {
+      name:"Learning Typescript",
+      exerciseCount:27,
+      description:'Lol'
     }
   ];
-
+const assertNever=(value:never):never=>{
+  throw new Error(
+    `Unhandled discriminated union member: ${JSON.stringify(value)}`
+  )
+}
   const Header:React.FC<{header:string}>=({header})=>(
     <h1>{header}</h1>
   );
-const Content:React.FC<CoursePartsProps>=(props)=>(
+const Content:React.FC<{courseParts:CoursePart[]}>=(props)=>(
   <> 
- {props.courseParts.map((part:any)=><p>{part.name} {part.exerciseCount}</p>)}
+ {props.courseParts.map((part)=><Part key={part.name} part={part}/>)}
   </>)
 ;
 
@@ -32,6 +46,50 @@ const Total:React.FC<CoursePartsProps>=(props)=>(
 <p>Number of exercises {props.courseParts.reduce((carry, part) => carry + part.exerciseCount, 0)}</p>
 </>
 )
+
+const Part:React.FC<{part:CoursePart}>=({part})=>{
+    switch(part.name){
+      case 'Fundamentals':
+        return(
+          <div>
+            <li>
+            <p>{part.name}</p>
+            <p>{part.description}</p>
+            </li>
+          </div>
+        );
+        case 'Using props to pass data':
+          return(
+            <div>
+              <li>
+              <p>{part.name}</p>
+              <p>{part.groupProjectCount}</p>
+              </li>
+            </div>
+          );
+          case 'Deeper type usage':
+            return(
+              <div>
+                <li>
+                <p>{part.name}</p>
+                <p>Description: {part.description}</p>
+                <p>{part.exerciseSubmissionLink}</p>
+                </li>
+              </div>
+            );
+            case 'Learning Typescript':
+              return(
+                <div>
+                  <p>{part.name}</p>
+                  <p>{part.description}</p>
+                  <p>{part.exerciseCount}</p>
+                </div>
+              );
+            default:
+return assertNever(part);
+    }
+  }
+
   return (
     <div>
     <Header header={courseName}/>
--- coursests/src/types.ts
+++ coursests/src/types.ts
@@ -6,3 +6,30 @@ export interface CoursePartsProps{
 courseParts: Course[];
 }
 
+export interface CoursePartBase {
+    name: string;
+    description?:string;
+    exerciseCount: number;
+  }
+  
+ export interface CoursePartOne extends CoursePartBase {
+    name: "Fundamentals";
+    description: string;
+  }
+  
+ export interface CoursePartTwo extends CoursePartBase {
+    name: "Using props to pass data";
+    groupProjectCount: number;
+  }
+  
+ export interface CoursePartThree extends CoursePartBase {
+    name: "Deeper type usage";
+    description: string;
+    exerciseSubmissionLink: string;
+  }
+  export interface CoursePartFour extends CoursePartBase {
+    name: "Learning Typescript";
+    description: string;
+    exerciseCount:number;
+  }
+  export type CoursePart = CoursePartOne | CoursePartTwo | CoursePartThree|CoursePartFour;
